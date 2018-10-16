b91b738c37301e3bf708149d6cdd1ef75ded4503
Erin Green <erin.y.green@gmail.com>
2018-10-16 01:06:40 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,6 +6,20 @@ class Triangle
     @side_a = side_a
     @side_b = side_b
     @side_c = side_c 
+    
+    if 
+     (@side_a <= 0) ||
+     (@side_b <= 0) ||
+     (@side_c <= 0)
+      raise TriangleError
+    end
+    
+    if  
+     (@side_a + @side_b) <= @side_c ||
+     (@side_a + @side_c) <= @side_b ||
+     (@side_c + @side_b) <= @side_a
+        raise TriangleError
+      end
   end
   
   def kind 
@@ -16,23 +30,8 @@ class Triangle
       (@side_a == @side_c && @side_a != @side_b) ||
       (@side_b == @side_c && @side_b != @side_a)
       :isosceles
-    elsif 
+    else 
       @side_a != @side_b && @side_a != @side_c && @side_b != @side_c
       :scalene
-    elsif 
-     (@side_a <= 0) ||
-     (@side_b <= 0) ||
-     (@side_c <= 0)
-      raise TriangleError
-    else 
-     (@side_a + @side_b) <= @side_c ||
-     (@side_a + @side_c) <= @side_b ||
-     (@side_c + @side_b) <= @side_a
-        raise TriangleError
-      end
+    end
   end
-  
-  class TriangleError < StandardError
-  end
-  
-end
